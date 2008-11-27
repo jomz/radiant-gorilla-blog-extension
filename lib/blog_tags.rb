2bfee205021940bd267809f1f3da728f0e19279e
Benny Degezelle <benny@gorilla-webdesign.be>
2008-11-27 21:33:16 +0100

    fixing sitelang issue, enhancing radius stuff

--- lib/blog_tags.rb
+++ lib/blog_tags.rb
@@ -1,5 +1,7 @@
 module BlogTags
   include Radiant::Taggable
+  include ActionView::Helpers::TagHelper # to pass html attributes through tags
+  require "digest" # for r:gravatar
 
   class TagError < StandardError; end
   
@@ -7,7 +9,7 @@ module BlogTags
     Render a list of blog items. Finds all articles for a blog, and provides links to them in a workable markup.
     <strong>Usage:</strong>
     
-    <pre><code><r:archive_list blog="/news" [hmtl_id="archive"] [mention_author="false"]/></code></pre>
+    <pre><code><r:archive_list blog="/news" [mention_author="false"] [html_attributes]/></code></pre>
     
     <strong>Attributes:</strong>
     
@@ -17,7 +19,7 @@ module BlogTags
     
     <strong>Example output:</strong>
     
-    <pre><ul class="archive">
+    <pre><ul class="archive_article_list">
       <li>2008<ul class="months">
           <li>February<ul class="articles">
               <li><a href="/news/2008/02/30/article-title/">Article breadcrumb</a></li>
@@ -30,21 +32,22 @@ module BlogTags
   
   tag "archive_list" do |tag|
     raise TagError, "archive_list tag requires a 'blog' attribute." unless tag.attr['blog']
-    html_id = !tag.attr['html_id'].nil? ? " id=\"#{tag.attr['html_id'] }\"" : ""
-    mention_author = true unless tag.attr['mention_author'] && tag.attr['mention_author'] == 'false'
+    mention_author = true unless tag.attr.delete('mention_author') == 'false'
+    by = tag.attr.delete('by') || 'by'
+    on = tag.attr.delete('on') || 'on'
     list = ""
     # readyup contents
-    blog = Page.find_by_url tag.attr['blog']
+    blog = Page.find_by_url tag.attr.delete('blog')
     blog.tree_children.each do |year|
-      list << "<li>#{year.title}<ul class=\"months\">"
+      list << "<li><h3>#{year.title}</h3><ul class=\"months\">"
       year.tree_children.each do |month|
         published = month.tree_children.delete_if{|c| !c.published? }
         if published.length > 0
-          list << "<li>#{month.title} <span class=\"amount\">(#{published.length})</span><ul class=\"articles\">"
+          list << "<li><h4>#{month.title} <span class=\"amount\">(#{published.length})</span></h4><ul class=\"articles\">"
             published.each do |article|
               list << "<li><a href=\"#{article.url}\">#{article.title}</a><span class=\"metadata\"> "
-              list << "door <span class=\"author\">#{article.created_by.name}</span> " if mention_author
-              list << "op <span class=\"date\">#{article.published_at.strftime("%d/%m/%Y")}</span>"
+              list << "#{by} <span class=\"author\">#{article.created_by.name}</span> " if mention_author
+              list << "#{on} <span class=\"date\">#{article.published_at.strftime("%d/%m/%Y")}</span>"
               list << "</span></li>"
             end
           list << "</ul></li>"
@@ -52,16 +55,25 @@ module BlogTags
       end
       list << "</ul></li>"
     end
-    # output
-    %{<ul#{html_id} class="archive">
+    # pass html attributes
+    if tag.attr
+      html_options = tag.attr.stringify_keys
+      tag_options = tag_options(html_options)
+    else
+      tag_options = nil
+    end
+    
+    %{<ul#{tag_options}>
       #{list}
     </ul>}
   end
   
   desc %{
-    <r:archive_items_in url="" />
+    Collects all Archive children in a certain scope
+    
+    <pre><code><r:archive_items_in url="" />
       // works as children:each..
-    </r:archive_items_in>
+    </r:archive_items_in></code></pre>
   }
   
   tag "archive_items_in" do |tag|
@@ -83,8 +95,11 @@ module BlogTags
     result
   end
   
-  desc %{ Run through escape_once
+  desc %{ 
+    Run through escape_once
+    
     <strong>Usage:</strong>
+    
     <pre><code><r:escape><r:title /></r:escape></code></pre>
   }
   
@@ -94,10 +109,14 @@ module BlogTags
   
   ## stolen from search extension
   
-  desc %{    <r:truncate_and_strip [length="100"] />
+  desc %{
     Truncates and strips all HTML tags from the content of the contained block.  
     Useful for displaying a snippet of a found page.  The optional `length' attribute
     specifies how many characters to truncate to.
+    
+    <strong>Usage:</strong>
+    
+    <pre><code><r:truncate_and_strip [length="100"] /></code></pre>
   }
 
   tag 'truncate_and_strip' do |tag|
@@ -153,6 +172,22 @@ module BlogTags
   
   tag "time_ago_in_words" do |tag|
     ActionView::Base.new.time_ago_in_words(tag.locals.page.published_at || tag.locals.page.created_at)
+  end
+  
+  tag "gravatar" do |tag|
+    email = tag.expand
+    raise TagError, "Gravatar tag requires an email attribute" if email.blank?
+    # include MD5 gem, should be part of standard ruby install
+    hash = Digest::MD5.hexdigest(email.downcase)
+    
+    if tag.attr
+      html_options = tag.attr.stringify_keys
+      tag_options = tag_options(html_options)
+    else
+      tag_options = nil
+    end
+    
+    "<img src=\"http://www.gravatar.com/avatar/#{hash}\" #{tag_options}/>"
   end
   
   private
